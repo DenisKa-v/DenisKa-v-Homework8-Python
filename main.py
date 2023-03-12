e1ea0a680b1c4397ef465bad4e689561a2653919
DenisKa <pochta_dk@mail.ru>
2023-03-12 23:52:48 +0300

    Add task2

--- main.py
+++ main.py
@@ -31,3 +31,39 @@
 #     print('Некорректный ввод! Введите положительное целое число.')
 # else:
 #     read_last(int(read_lines), read_file)
+
+"""2. Документ «article.txt» содержит следующий текст:
+Вечерело
+Жужжали мухи
+Светил фонарик
+Кипела вода в чайнике
+Венера зажглась на небе
+Деревья шумели
+Тучи разошлись
+Листва зеленела
+
+Требуется реализовать функцию longest_words(file), которая выводит слово, имеющее максимальную длину 
+(или список слов, если таковых несколько)."""
+
+# def longest_words(file):
+#     with open(file, 'r', encoding='utf-8') as file:
+#         text = file.read().split()
+#     if not text:
+#         print(f'Файл {file.name} пустой.')
+#     else:
+#         max_el = 0
+#         text2 = set()
+#         for el in text:
+#             if len(el) > max_el:
+#                 max_el = len(el)
+#         for elm in text:
+#             if len(elm) == max_el:
+#                 text2.add(elm)
+#         print(f'Слово или слова с максимальной длиной (равной {max_el}) из файла {file.name}:')
+#         for i in text2:
+#             print(i)
+#         print(f'Количество, найденный слов: {len(text2)}.')
+#
+#
+# read_file = 'article.txt'
+# longest_words(read_file)
